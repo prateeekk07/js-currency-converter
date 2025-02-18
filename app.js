64cf572ba1e4b804bb9579602067fdd6bbe94efd
Test Demo <prateekc53@gmail.com>
2025-02-18 20:29:16 +0530

    added js for dropdown

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("hello");
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+for(let select of dropdowns){
+    for (currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if (select.name === "from" && currCode === "USD"){
+            newOption.selected = "selected";
+        } else if (select.name === "to" && currCode === "INR"){
+            newOption.selected = "selected";
+            }
+         select.append(newOption);
+    }
+}
+
+ 
